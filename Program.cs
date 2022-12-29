4a42a0e63fb30a32b18cf6a400312ce2d8d34b52
Guilherme Rodrigues <guilherme-rodrigues75@hotmail.com>
2022-12-29 14:43:35 -0300

    Alterações definitivas do desafio concluido

--- Program.cs
+++ Program.cs
@@ -2,15 +2,17 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
-Console.WriteLine("Smarphone Motorola");
+Console.WriteLine("Smarphone Nokia");
 
 Smartphone nokia = new Nokia(numero: "984160795", modelo: "Motoe",iMEI:"358147", memoria: 32);
 nokia.Ligar();
+nokia.ReceberLigacao();
 nokia.InstalarAplicativo("WhatsApp");
 
 Console.WriteLine("\n");
 
 Console.WriteLine("Iphone");
 Iphone iphone = new Iphone(numero:"984827646",modelo:"Iphone 7",iMEI: "111111",memoria: 32);
+iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");

7f1db78dbcea79c9338aacb1745c2925d70f267e
Guilherme Rodrigues <guilherme.abreu@apac.local>
2022-12-29 14:19:30 -0300

    Desafio P.O.O concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smarphone Motorola");
+
+Smartphone nokia = new Nokia(numero: "984160795", modelo: "Motoe",iMEI:"358147", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone(numero:"984827646",modelo:"Iphone 7",iMEI: "111111",memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
